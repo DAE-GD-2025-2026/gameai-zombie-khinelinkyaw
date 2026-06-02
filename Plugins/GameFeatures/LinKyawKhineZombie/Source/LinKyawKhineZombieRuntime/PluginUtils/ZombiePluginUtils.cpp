9f1aa7ba8aa295773350cad411f14d7b9b858a39
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-02 15:21:29 +0200

    Some utils for the plugin

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.cpp
@@ -0,0 +1,23 @@
+﻿#include "ZombiePluginUtils.h"
+
+TArray<TObjectPtr<ABaseItem>> PluginUtils::GetAllItemsInHouse(AHouse const* House,
+	TSet<TObjectPtr<ABaseItem>> Items)
+{
+	TArray<TObjectPtr<ABaseItem>> Result{};
+	
+	FVector HouseOrigin{}, HouseBoxExtend{};
+	House->GetActorBounds(false, HouseOrigin, HouseBoxExtend);
+	FBox const HouseBox { FBox::BuildAABB(HouseOrigin, HouseBoxExtend) };
+	
+	for (auto const Item : Items)
+	{
+		auto ItemLocation {Item->GetActorLocation()};
+		ItemLocation.Z += 1;
+		if (HouseBox.IsInside(ItemLocation))
+		{
+			Result.Add(Item);
+		}
+	}
+	
+	return Result;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include "Items/BaseItem.h"
+#include "Village/House/House.h"
+
+namespace PluginUtils
+{
+	TArray<TObjectPtr<ABaseItem>> GetAllItemsInHouse(AHouse const* House, TSet<TObjectPtr<ABaseItem>> Items);
+}
