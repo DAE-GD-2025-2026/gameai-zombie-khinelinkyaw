29799f121b6321d2087e61f013b2bb543ea8b754
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-02 15:20:08 +0200

    Decorator to check if items are in the house

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_AreItemsInHouse.cpp
@@ -0,0 +1,51 @@
+﻿#include "BTDecorator_AreItemsInHouse.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+#include <GameAI_Zombie/Village/House/House.h>
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+UBTDecorator_AreItemsInHouse::UBTDecorator_AreItemsInHouse()
+{
+	NodeName = "AreItemsInHouse";
+	HouseKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTDecorator_AreItemsInHouse, HouseKey), AHouse::StaticClass());
+	SurvivorKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTDecorator_AreItemsInHouse, SurvivorKey), AActor::StaticClass());
+}
+
+bool UBTDecorator_AreItemsInHouse::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory) const
+{
+	UBlackboardComponent const* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	
+	if (!BlackboardComp)
+	{
+		return false;
+	}
+	
+	AHouse const* House = Cast<AHouse>(BlackboardComp->GetValueAsObject(HouseKey.SelectedKeyName));
+	AActor const* Survivor = Cast<AActor>(BlackboardComp->GetValueAsObject(SurvivorKey.SelectedKeyName));
+	
+	if (House == nullptr or Survivor == nullptr) return false;
+	
+	UStudentPerceptor* PerceptorComp { Survivor->GetComponentByClass<UStudentPerceptor>() };
+	
+	FVector HouseOrigin{}, HouseBoxExtend{};
+	House->GetActorBounds(false, HouseOrigin, HouseBoxExtend);
+	FBox const HouseBox { FBox::BuildAABB(HouseOrigin, HouseBoxExtend) };
+	
+	auto const PerceivedItems{ PerceptorComp->GetPerceivedItems() };
+	
+	for (auto const Item : PerceivedItems)
+	{
+		auto ItemLocation {Item->GetActorLocation()};
+		ItemLocation.Z += 1;
+		if (HouseBox.IsInside(ItemLocation))
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_AreItemsInHouse.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_AreItemsInHouse.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_AreItemsInHouse : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector HouseKey;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector SurvivorKey;
+	
+public:
+	UBTDecorator_AreItemsInHouse();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+	
+};
