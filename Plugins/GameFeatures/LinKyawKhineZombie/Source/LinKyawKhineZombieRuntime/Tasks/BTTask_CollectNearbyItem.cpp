9328ed4dd87ffa524c6f2dde3c94da34ff15ab99
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-02 15:20:44 +0200

    Decorator to set a nearby item to collect

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_CollectNearbyItem.cpp
@@ -0,0 +1,52 @@
+﻿#include "BTTask_CollectNearbyItem.h"
+
+#include <../PluginUtils/ZombiePluginUtils.h>
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+#include <GameAI_Zombie/Village/House/House.h>
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+
+
+UBTTask_CollectNearbyItem::UBTTask_CollectNearbyItem()
+{
+	NodeName = "Collect Nearby Item";
+	HouseKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_CollectNearbyItem, HouseKey), AHouse::StaticClass());
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_CollectNearbyItem, BlackboardKey), ABaseItem::StaticClass());
+}
+
+EBTNodeResult::Type UBTTask_CollectNearbyItem::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn())
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	UBlackboardComponent const* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	
+	if (!BlackboardComp)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	AHouse const* House = Cast<AHouse>(BlackboardComp->GetValueAsObject(HouseKey.SelectedKeyName));
+	FVector SurvivorLocation = AIController->GetPawn()->GetActorLocation();
+	
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	
+	auto ItemsInHouse { PluginUtils::GetAllItemsInHouse(House, PerceptorComp->GetPerceivedItems()) };
+	
+	if (ItemsInHouse.IsEmpty())
+	{
+		return EBTNodeResult::Failed;
+	}
+	else
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsObject(BlackboardKey.SelectedKeyName, ItemsInHouse[0]);
+		return EBTNodeResult::Succeeded;
+	}
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_CollectNearbyItem.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_CollectNearbyItem.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_CollectNearbyItem final : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector HouseKey;
+	
+public:
+	UBTTask_CollectNearbyItem();
+	 
+	// This is the main logic of your task
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
