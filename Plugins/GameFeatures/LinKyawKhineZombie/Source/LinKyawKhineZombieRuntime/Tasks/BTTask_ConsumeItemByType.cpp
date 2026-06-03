f4fe83902c7ec18fdd3addb20157f4de3a7b9ed4
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 14:01:30 +0200

    Task for consuming an item from the inventory

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_ConsumeItemByType.cpp
@@ -0,0 +1,35 @@
+﻿#include "BTTask_ConsumeItemByType.h"
+
+#include <LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h>
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+UBTTask_ConsumeItemByType::UBTTask_ConsumeItemByType()
+{
+	NodeName = "Consume Item By Type";
+}
+
+EBTNodeResult::Type UBTTask_ConsumeItemByType::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return EBTNodeResult::Failed;
+
+	UInventoryComponent* InvComp { AIController->GetPawn()->GetComponentByClass<UInventoryComponent>() };
+	
+	if (int FirstIndex {PluginUtils::GetFirstItemByType(*InvComp, ItemType)}; FirstIndex != -1)
+	{
+		if (InvComp->UseItem(FirstIndex))
+		{
+			return EBTNodeResult::Succeeded;
+		}
+		else
+		{
+			return EBTNodeResult::Aborted;
+		}
+	}
+	
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_ConsumeItemByType.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+
+#include <GameAI_Zombie/Items/ItemType.h>
+
+#include "BTTask_ConsumeItemByType.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_ConsumeItemByType final : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Item")
+	EItemType ItemType;
+	
+public:
+	UBTTask_ConsumeItemByType();
+	 
+	// This is the main logic of your task
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
