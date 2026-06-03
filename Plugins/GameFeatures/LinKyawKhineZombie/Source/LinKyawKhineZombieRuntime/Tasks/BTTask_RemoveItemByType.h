5c6d733cfd552e042c5344d176735c8d7b80e05f
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 14:01:45 +0200

    Task for removing an item from the inventory

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_RemoveItemByType.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+
+#include <GameAI_Zombie/Items/ItemType.h>
+
+#include "BTTask_RemoveItemByType.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_RemoveItemByType final : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Item")
+	EItemType ItemType;
+	
+public:
+	UBTTask_RemoveItemByType();
+	 
+	// This is the main logic of your task
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
