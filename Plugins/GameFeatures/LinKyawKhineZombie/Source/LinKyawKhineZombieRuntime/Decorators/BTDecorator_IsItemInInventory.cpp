513ccec9336d3c5b55b903eb40e8a8e1c2b0c3a5
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:07:48 +0200

    Decorator for checking is an item is in the inventory

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsItemInInventory.cpp
@@ -0,0 +1,26 @@
+﻿#include "BTDecorator_IsItemInInventory.h"
+
+#include <LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h>
+
+#include "AIController.h"
+
+UBTDecorator_IsItemInInventory::UBTDecorator_IsItemInInventory()
+{
+	NodeName = "Is Item In Inventory";
+}
+
+bool UBTDecorator_IsItemInInventory::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+
+	UInventoryComponent const* InvComp { AIController->GetPawn()->GetComponentByClass<UInventoryComponent>() };
+	
+	if (PluginUtils::GetFirstItemByType(*InvComp, ItemType) != -1)
+	{
+		return true;
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsItemInInventory.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+
+#include <GameAI_Zombie/Items/ItemType.h>
+
+#include "BTDecorator_IsItemInInventory.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_IsItemInInventory : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	EItemType ItemType;
+
+	
+public:
+	UBTDecorator_IsItemInInventory();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
