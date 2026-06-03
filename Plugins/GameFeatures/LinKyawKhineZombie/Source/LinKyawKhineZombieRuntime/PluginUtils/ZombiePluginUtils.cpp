1e0cc122ef825e65413d983e3f1f1be7ecfb8618
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 14:02:17 +0200

    A util function to get the first item of type from inventory

--- Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.cpp
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.cpp
@@ -41,3 +41,40 @@ int PluginUtils::GetFirstEmptyInventoryIndex(UInventoryComponent const* InvComp)
 	
 	return -1;
 }
+
+bool PluginUtils::ConsumeItemByType(UInventoryComponent const& InvComp, EItemType ItemType, ASurvivorPawn& SurvivorPawn)
+{
+	auto Items { InvComp.GetInventory() };
+	int InventorySize { InvComp.GetInventoryCapacity() };
+	
+	for (int Index{0}; Index < InventorySize; ++Index)
+	{
+		if (Items[Index] == nullptr) continue;
+		
+		if (Items[Index]->GetItemType() == ItemType)
+		{
+			Items[Index]->UseItem(SurvivorPawn);
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+int PluginUtils::GetFirstItemByType(UInventoryComponent const& InvComp, EItemType ItemType)
+{
+	auto Items { InvComp.GetInventory() };
+	int InventorySize { InvComp.GetInventoryCapacity() };
+	
+	for (int Index{0}; Index < InventorySize; ++Index)
+	{
+		if (Items[Index] == nullptr) continue;
+		
+		if (Items[Index]->GetItemType() == ItemType)
+		{
+			return Index;
+		}
+	}
+	
+	return -1;
+}
--- Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h
@@ -1,10 +1,15 @@
 ﻿#pragma once
 
 #include "Items/BaseItem.h"
+#include "Common/InventoryComponent.h"
 #include "Village/House/House.h"
 
 namespace PluginUtils
 {
 	TArray<TObjectPtr<ABaseItem>> GetAllItemsInHouse(AHouse const* House, TSet<TObjectPtr<ABaseItem>> Items);
 	int GetFirstEmptyInventoryIndex(UInventoryComponent const* InvComp);
+	
+	bool ConsumeItemByType(UInventoryComponent const& InvComp, EItemType ItemType, ASurvivorPawn& SurvivorPawn);
+	
+	int GetFirstItemByType(UInventoryComponent const& InvComp, EItemType ItemType);
 }
