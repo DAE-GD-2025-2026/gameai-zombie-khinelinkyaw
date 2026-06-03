19522f9815cec28058f52b6ae2559dc880db1944
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:06:00 +0200

    Task to face the survivor to an actor (Zombie)

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_FaceActor.cpp
@@ -0,0 +1,28 @@
+﻿#include "BTTask_FaceActor.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_FaceActor::UBTTask_FaceActor()
+{
+	NodeName = "Face Actor";
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_FaceActor, BlackboardKey), AActor::StaticClass());
+	RotatorKey.AddRotatorFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_FaceActor, RotatorKey));
+}
+
+EBTNodeResult::Type UBTTask_FaceActor::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return EBTNodeResult::Failed;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return EBTNodeResult::Failed;
+	
+	if (AActor* ActorToAvoid = Cast<AActor>(BlackboardComp->GetValueAsObject(BlackboardKey.SelectedKeyName)))
+	{
+		AIController->SetFocus(ActorToAvoid);
+		return EBTNodeResult::Succeeded;
+	}
+	
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_FaceActor.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+
+#include "BTTask_FaceActor.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_FaceActor : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector RotatorKey;
+	
+public:
+	UBTTask_FaceActor();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
