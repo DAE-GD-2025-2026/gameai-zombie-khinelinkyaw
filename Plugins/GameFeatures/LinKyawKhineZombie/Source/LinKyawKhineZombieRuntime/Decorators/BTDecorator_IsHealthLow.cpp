1436387159529ae27d90936af1d6d71d515296e2
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:08:06 +0200

    Decorator for checking if the health is low

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsHealthLow.cpp
@@ -0,0 +1,23 @@
+﻿#include "BTDecorator_IsHealthLow.h"
+
+#include <GameAI_Zombie/Common/HealthComponent.h>
+
+#include "AIController.h"
+
+UBTDecorator_IsHealthLow::UBTDecorator_IsHealthLow()
+{
+	NodeName = "Is Health Low";
+	Threshold = 5;
+}
+
+bool UBTDecorator_IsHealthLow::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	if (UHealthComponent const* StaminaComp { AIController->GetPawn()->GetComponentByClass<UHealthComponent>() })
+	{
+		return StaminaComp->GetHealth() < Threshold;
+	}
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsHealthLow.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_IsHealthLow.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_IsHealthLow : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	int Threshold;
+	
+public:
+	UBTDecorator_IsHealthLow();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
