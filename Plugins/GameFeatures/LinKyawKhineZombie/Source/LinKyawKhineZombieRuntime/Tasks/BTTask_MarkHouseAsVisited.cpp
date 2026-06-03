ebe0400b2bce0dbeb430d2fd677fb50939937989
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 12:36:45 +0200

    Task for marking a house as visited so that player don't get stuck indefinitely

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_MarkHouseAsVisited.cpp
@@ -0,0 +1,41 @@
+﻿#include "BTTask_MarkHouseAsVisited.h"
+
+#include <../PluginUtils/ZombiePluginUtils.h>
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+UBTTask_MarkHouseAsVisited::UBTTask_MarkHouseAsVisited()
+{
+	NodeName = "Mark House as Visited";
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_MarkHouseAsVisited, BlackboardKey), AHouse::StaticClass());
+}
+
+EBTNodeResult::Type UBTTask_MarkHouseAsVisited::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn())
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	
+	if (!BlackboardComp)
+	{
+		return EBTNodeResult::Failed;
+	}
+	FVector SurvivorLocation = AIController->GetPawn()->GetActorLocation();
+	
+	AHouse* House = Cast<AHouse>(BlackboardComp->GetValueAsObject(BlackboardKey.SelectedKeyName));
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	
+	if (House)
+	{
+		PerceptorComp->MarkHouseAsVisisted(House);
+		BlackboardComp->SetValueAsObject(BlackboardKey.SelectedKeyName, nullptr);
+		return EBTNodeResult::Succeeded;
+	}
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_MarkHouseAsVisited.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_MarkHouseAsVisited.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_MarkHouseAsVisited final : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_MarkHouseAsVisited();
+	 
+	// This is the main logic of your task
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
