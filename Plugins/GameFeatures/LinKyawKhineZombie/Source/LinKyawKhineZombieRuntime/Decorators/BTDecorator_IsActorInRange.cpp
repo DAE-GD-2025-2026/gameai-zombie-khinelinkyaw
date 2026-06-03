e6de721663dba7e44c1fc3e8ed99a0ff45080d1e
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:08:24 +0200

    Decorator for checking if an actor (zombie) is in range

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsActorInRange.cpp
@@ -0,0 +1,30 @@
+﻿#include "BTDecorator_IsActorInRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTDecorator_IsActorInRange::UBTDecorator_IsActorInRange()
+{
+	NodeName = "Is Zombie In Range";
+	ActorKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTDecorator_IsActorInRange, ActorKey), AActor::StaticClass());
+}
+
+bool UBTDecorator_IsActorInRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return false;
+	
+	if (AActor const* ActorToAvoid = Cast<AActor>(BlackboardComp->GetValueAsObject(ActorKey.SelectedKeyName)))
+	{
+		FVector const ActorLocation { ActorToAvoid->GetActorLocation() };
+		FVector const SurvivorPosition { AIController->GetPawn()->GetActorLocation() };
+		
+		return (SurvivorPosition - ActorLocation).Size() < Range;
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsActorInRange.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_IsActorInRange.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_IsActorInRange : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector ActorKey;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	float Range;
+	
+public:
+	UBTDecorator_IsActorInRange();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
