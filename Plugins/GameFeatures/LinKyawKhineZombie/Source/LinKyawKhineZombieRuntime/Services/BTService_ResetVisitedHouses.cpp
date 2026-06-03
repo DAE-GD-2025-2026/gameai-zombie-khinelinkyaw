711fb32a5fc40b52f760402d97dd73e45ac6ebf8
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:03:51 +0200

    Service for resetting visited houses

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Services/BTService_ResetVisitedHouses.cpp
@@ -0,0 +1,23 @@
+﻿#include "BTService_ResetVisitedHouses.h"
+
+#include "AIController.h"
+
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+UBTService_ResetVisitedHouses::UBTService_ResetVisitedHouses()
+{
+	NodeName = "Reset Visited Houses";
+}
+
+void UBTService_ResetVisitedHouses::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return;
+
+	if (UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() })
+	{
+		PerceptorComp->ClearVisitedHouses();
+	}
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Services/BTService_ResetVisitedHouses.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_ResetVisitedHouses.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTService_ResetVisitedHouses final : public UBTService
+{
+	GENERATED_BODY()
+public:
+	UBTService_ResetVisitedHouses();
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
