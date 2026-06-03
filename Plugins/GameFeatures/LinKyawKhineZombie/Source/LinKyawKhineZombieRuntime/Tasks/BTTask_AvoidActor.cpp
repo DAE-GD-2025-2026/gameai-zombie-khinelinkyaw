315c3746f62896e4feb0439caf4ca5a916e0f934
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:03:33 +0200

    Task for avoiding actor (zombies)

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_AvoidActor.cpp
@@ -0,0 +1,36 @@
+﻿#include "BTTask_AvoidActor.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_AvoidActor::UBTTask_AvoidActor()
+{
+	NodeName = "Avoid Actor";
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_AvoidActor, BlackboardKey), AActor::StaticClass());
+	DestinationKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_AvoidActor, DestinationKey));
+}
+
+EBTNodeResult::Type UBTTask_AvoidActor::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return EBTNodeResult::Failed;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return EBTNodeResult::Failed;
+	
+	if (AActor const* ActorToAvoid = Cast<AActor>(BlackboardComp->GetValueAsObject(BlackboardKey.SelectedKeyName)))
+	{
+		FVector const ActorLocation { ActorToAvoid->GetActorLocation() };
+		FVector const SurvivorPosition { AIController->GetPawn()->GetActorLocation() };
+		FVector const AvoidLocation {
+			SurvivorPosition.X + ActorLocation.X,
+			SurvivorPosition.Y + ActorLocation.Y,
+			SurvivorPosition.Z
+		};
+		
+		BlackboardComp->SetValueAsVector(DestinationKey.SelectedKeyName, AvoidLocation);
+		return EBTNodeResult::Succeeded;
+	}
+	
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_AvoidActor.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+
+#include "BTTask_AvoidActor.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_AvoidActor final : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector DestinationKey;
+	
+public:
+	UBTTask_AvoidActor();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
