2ea379aac5b328ee5c88ba005dbf8d0bf3dae38a
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 20:06:34 +0200

    Decorator for checking if the stamina is low

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsStaminaLow.cpp
@@ -0,0 +1,23 @@
+﻿#include "BTDecorator_IsStaminaLow.h"
+
+#include <GameAI_Zombie/Common/StaminaComponent.h>
+
+#include "AIController.h"
+
+UBTDecorator_IsStaminaLow::UBTDecorator_IsStaminaLow()
+{
+	NodeName = "Is Stamina Low";
+	Threshold = 5;
+}
+
+bool UBTDecorator_IsStaminaLow::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	if (UStaminaComponent const* StaminaComp { AIController->GetPawn()->GetComponentByClass<UStaminaComponent>() })
+	{
+		return StaminaComp->GetCurrentStamina() < Threshold;
+	}
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsStaminaLow.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_IsStaminaLow.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_IsStaminaLow : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	int Threshold;
+	
+public:
+	UBTDecorator_IsStaminaLow();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
