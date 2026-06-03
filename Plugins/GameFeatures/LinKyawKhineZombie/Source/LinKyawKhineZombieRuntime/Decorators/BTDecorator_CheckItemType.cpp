06844e357e575487c308e1e66424255277a8c651
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-03 12:36:23 +0200

    Decorator for checking an Item type from a Blackboard key

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_CheckItemType.cpp
@@ -0,0 +1,23 @@
+﻿#include "BTDecorator_CheckItemType.h"
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include <BehaviorTree/BlackboardComponent.h>
+
+UBTDecorator_CheckItemType::UBTDecorator_CheckItemType()
+{
+	NodeName = "Check Item Type";
+	ItemKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTDecorator_CheckItemType, ItemKey), ABaseItem::StaticClass());
+}
+
+bool UBTDecorator_CheckItemType::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	UBlackboardComponent const* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return false;
+	
+	ABaseItem const* Item = Cast<ABaseItem>(BlackboardComp->GetValueAsObject(ItemKey.SelectedKeyName));
+	
+	if (!Item) return false;
+	
+	return Item->GetItemType() == ItemType;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_CheckItemType.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include <GameAI_Zombie/Items/ItemType.h>
+
+#include "BTDecorator_CheckItemType.generated.h"
+
+UCLASS()
+class UBTDecorator_CheckItemType : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector ItemKey;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	EItemType ItemType;
+	
+public:
+	UBTDecorator_CheckItemType();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
