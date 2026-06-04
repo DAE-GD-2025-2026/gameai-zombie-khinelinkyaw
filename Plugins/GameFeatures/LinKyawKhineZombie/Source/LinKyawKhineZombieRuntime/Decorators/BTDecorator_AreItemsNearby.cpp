437c9169e9f113f892f9ef9ef85fc80026e0ff03
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-04 19:51:05 +0200

    Decorator for checking if there are items nearby (range can be adjusted)

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_AreItemsNearby.cpp
@@ -0,0 +1,39 @@
+﻿#include "BTDecorator_AreItemsNearby.h"
+
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+UBTDecorator_AreItemsNearby::UBTDecorator_AreItemsNearby()
+{
+	NodeName = "Are Items Nearby";
+	Range = 100.f;
+}
+
+bool UBTDecorator_AreItemsNearby::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return false;
+	
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	auto Items { PerceptorComp->GetPerceivedItems() };
+	
+	FVector const SurvivorLocation { AIController->GetPawn()->GetActorLocation() };
+	for (auto const Item : Items)
+	{
+		double const Distance { (Item->GetActorLocation() - SurvivorLocation).Size() };
+		
+		if (Distance < Range)
+		{
+			return true;			
+		}
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_AreItemsNearby.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_AreItemsNearby.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_AreItemsNearby : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	float Range;
+	
+public:
+	UBTDecorator_AreItemsNearby();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+	
+};
