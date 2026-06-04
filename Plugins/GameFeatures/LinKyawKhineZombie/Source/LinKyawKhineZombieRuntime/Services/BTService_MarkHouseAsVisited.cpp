d6bf54c1de7256c8db7f581398bb50f596ad0147
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-04 19:52:05 +0200

    Service to mark a house as visited

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Services/BTService_MarkHouseAsVisited.cpp
@@ -0,0 +1,33 @@
+﻿#include "BTService_MarkHouseAsVisited.h"
+
+#include <GameAI_Zombie/Village/House/House.h>
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+UBTService_MarkHouseAsVisited::UBTService_MarkHouseAsVisited()
+{
+	NodeName = "Mark House as Visited";
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTService_MarkHouseAsVisited, BlackboardKey), AHouse::StaticClass());
+}
+
+void UBTService_MarkHouseAsVisited::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return;
+
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return;
+	
+	AHouse* House = Cast<AHouse>(BlackboardComp->GetValueAsObject(BlackboardKey.SelectedKeyName));
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	
+	if (House)
+	{
+		PerceptorComp->MarkHouseAsVisited(House);
+		BlackboardComp->SetValueAsObject(BlackboardKey.SelectedKeyName, nullptr);
+	}
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Services/BTService_MarkHouseAsVisited.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "BehaviorTree/BTService.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_MarkHouseAsVisited.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTService_MarkHouseAsVisited final : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+public:
+	UBTService_MarkHouseAsVisited();
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
