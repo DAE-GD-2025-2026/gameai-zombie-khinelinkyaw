354f55eedb91217089a98180c6d6481ca9e57063
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-04 19:51:33 +0200

    Decorator to check if the inventory is full

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsInventoryFull.cpp
@@ -0,0 +1,33 @@
+﻿#include "BTDecorator_IsInventoryFull.h"
+
+#include <LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h>
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTDecorator_IsInventoryFull::UBTDecorator_IsInventoryFull()
+{
+	NodeName = "Is Inventory Full";
+}
+
+bool UBTDecorator_IsInventoryFull::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+                                                              uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	UBlackboardComponent const* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return false;
+
+	UInventoryComponent const* InvComp { AIController->GetPawn()->GetComponentByClass<UInventoryComponent>() };
+	auto InvItems { InvComp->GetInventory() };
+	
+	for (auto const Item : InvItems)
+	{
+		if (Item == nullptr) return false;
+	}
+	
+	return true;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_IsInventoryFull.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Decorators/BTDecorator_Blackboard.h"
+
+#include "BTDecorator_IsInventoryFull.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_IsInventoryFull : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+public:
+	UBTDecorator_IsInventoryFull();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
