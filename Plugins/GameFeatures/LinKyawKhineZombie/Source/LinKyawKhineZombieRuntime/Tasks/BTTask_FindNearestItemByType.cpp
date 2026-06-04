b3a44684fc02aa4048ec0514ec53e2f19ae30781
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-04 19:52:34 +0200

    Task to find the nearest item by type

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_FindNearestItemByType.cpp
@@ -0,0 +1,52 @@
+﻿#include "BTTask_FindNearestItemByType.h"
+
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_FindNearestItemByType::UBTTask_FindNearestItemByType()
+{
+	NodeName = "Find Nearest Item By Type";
+	Range = 100.f;
+	ItemType = EItemType::Food;
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_FindNearestItemByType, BlackboardKey), ABaseItem::StaticClass());
+}
+
+EBTNodeResult::Type UBTTask_FindNearestItemByType::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return EBTNodeResult::Failed;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return EBTNodeResult::Failed;
+	
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	auto Items { PerceptorComp->GetPerceivedItemsByType(ItemType) };
+	
+	if (Items.IsEmpty()) return EBTNodeResult::Failed;
+	
+	FVector const SurvivorLocation { AIController->GetPawn()->GetActorLocation() };
+	
+	auto const ClosestElement { Algo::MinElementBy(Items, [SurvivorLocation](TObjectPtr<ABaseItem> const& Item)
+	{
+		FVector const ItemLocation{ Item->GetActorLocation() - SurvivorLocation };
+		return ItemLocation.Size();
+	})};
+	
+	if (ClosestElement)
+	{
+		FVector ClosestElementDistance { ClosestElement->Get()->GetActorLocation() - SurvivorLocation };
+		
+		if (ClosestElementDistance.Size() < Range)
+		{
+			BlackboardComp->SetValueAsObject(BlackboardKey.SelectedKeyName, ClosestElement->Get());
+			return EBTNodeResult::Succeeded;
+		}
+		
+	}
+	
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_FindNearestItemByType.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+
+#include <GameAI_Zombie/Items/ItemType.h>
+
+#include "BTTask_FindNearestItemByType.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_FindNearestItemByType final : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	float Range;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	EItemType ItemType;
+	
+public:
+	UBTTask_FindNearestItemByType();
+	 
+	// This is the main logic of your task
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
