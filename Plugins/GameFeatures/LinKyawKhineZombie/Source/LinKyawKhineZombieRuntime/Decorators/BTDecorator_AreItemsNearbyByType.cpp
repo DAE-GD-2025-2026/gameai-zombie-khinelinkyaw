dc437d50e8aaa606b3a76a915adc297dc21805d3
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-04 19:51:22 +0200

    Decorator for checking if there are items of a type nearby (range can be adjusted)

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_AreItemsNearbyByType.cpp
@@ -0,0 +1,41 @@
+﻿#include "BTDecorator_AreItemsNearbyByType.h"
+
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+UBTDecorator_AreItemsNearbyByType::UBTDecorator_AreItemsNearbyByType()
+{
+	NodeName = "Are Items Nearby By Type";
+	Range = 100.f;
+	ItemType = EItemType::Food;
+}
+
+bool UBTDecorator_AreItemsNearbyByType::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return false;
+	
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	auto Items { PerceptorComp->GetPerceivedItems() };
+	
+	FVector const SurvivorLocation { AIController->GetPawn()->GetActorLocation() };
+	for (auto const Item : Items)
+	{
+		double const Distance { (Item->GetActorLocation() - SurvivorLocation).Size() };
+		
+		if (Distance < Range and Item->GetItemType() == ItemType)
+		{
+			return true;			
+		}
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_AreItemsNearbyByType.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "BehaviorTree/BTDecorator.h"
+
+#include <GameAI_Zombie/Items/ItemType.h>
+
+#include "BTDecorator_AreItemsNearbyByType.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_AreItemsNearbyByType : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	float Range;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	EItemType ItemType;
+	
+public:
+	UBTDecorator_AreItemsNearbyByType();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+	
+};
