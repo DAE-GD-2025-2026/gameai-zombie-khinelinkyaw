ef0e4cc145b274f96b9ad659fe358328fea136b4
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-05 16:48:20 +0200

    Task for collecting item (Same as CollectNearbyItem)

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_CollectItem.cpp
@@ -0,0 +1,38 @@
+﻿#include "BTTask_CollectItem.h"
+
+#include <LinKyawKhineZombieRuntime/PluginUtils/ZombiePluginUtils.h>
+
+#include <GameAI_Zombie/Items/BaseItem.h>
+#include <GameAI_Zombie/Common/InventoryComponent.h>
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+
+UBTTask_CollectItem::UBTTask_CollectItem()
+{
+	NodeName = "Collect Item";
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_CollectItem, BlackboardKey), ABaseItem::StaticClass());
+}
+
+EBTNodeResult::Type UBTTask_CollectItem::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return EBTNodeResult::Failed;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return EBTNodeResult::Failed;
+
+	if (ABaseItem* Item = Cast<ABaseItem>(BlackboardComp->GetValueAsObject(BlackboardKey.SelectedKeyName)))
+	{
+		UInventoryComponent* InvComp { AIController->GetPawn()->GetComponentByClass<UInventoryComponent>() };
+
+		if (int FreeIndex{ PluginUtils::GetFirstEmptyInventoryIndex(InvComp) }; FreeIndex != -1)
+		{
+			BlackboardComp->SetValueAsObject(BlackboardKey.SelectedKeyName, nullptr);
+			InvComp->GrabItem(FreeIndex, Item);
+			return EBTNodeResult::Succeeded;
+		}
+	}
+	return EBTNodeResult::Failed;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Tasks/BTTask_CollectItem.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_CollectItem.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTTask_CollectItem final : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_CollectItem();
+	 
+	// This is the main logic of your task
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
