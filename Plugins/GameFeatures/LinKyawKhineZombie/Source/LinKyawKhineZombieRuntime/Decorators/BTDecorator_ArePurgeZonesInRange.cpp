656813bceb3ebc21811ca661c7042e1b1d21f624
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-05 16:48:05 +0200

    Decorator for checking if purge zones are in range

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_ArePurgeZonesInRange.cpp
@@ -0,0 +1,40 @@
+﻿#include "BTDecorator_ArePurgeZonesInRange.h"
+
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+#include <GameAI_Zombie/PurgeZones/PurgeZone.h>
+
+#include <AIController.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+UBTDecorator_ArePurgeZonesInRange::UBTDecorator_ArePurgeZonesInRange()
+{
+	NodeName = "Are Purge Zones in Range";
+	Range = 300.f;
+}
+
+bool UBTDecorator_ArePurgeZonesInRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory) const
+{
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return false;
+	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return false;
+	
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	auto PurgeZones { PerceptorComp->GetSortedPurgeZones() };
+	
+	FVector const SurvivorLocation { AIController->GetPawn()->GetActorLocation() };
+	for (auto const PurgeZone : PurgeZones)
+	{
+		double const Distance { (PurgeZone->GetActorLocation() - SurvivorLocation).Size() };
+		
+		if (Distance < Range)
+		{
+			return true;			
+		}
+	}
+	
+	return false;
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Decorators/BTDecorator_ArePurgeZonesInRange.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_ArePurgeZonesInRange.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTDecorator_ArePurgeZonesInRange : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	float Range;
+	
+public:
+	UBTDecorator_ArePurgeZonesInRange();
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+	
+};
