8a58428f2be761ee86349bb37a9092d613c8f64b
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-07 10:23:03 +0200

    Service for checking if purge zones are in range

---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Services/BTService_ArePurgeZonesInRange.cpp
@@ -0,0 +1,38 @@
+﻿#include "BTService_ArePurgeZonesInRange.h"
+
+#include <GameAI_Zombie/PurgeZones/PurgeZone.h>
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "LinKyawKhineZombieRuntime/StudentPerceptor.h"
+
+UBTService_ArePurgeZonesInRange::UBTService_ArePurgeZonesInRange()
+{
+	NodeName = "Are Purge Zones in Range";
+	BlackboardKey.AddBoolFilter(this, GET_MEMBER_NAME_CHECKED(UBTService_ArePurgeZonesInRange, BlackboardKey));
+	Range = 500.f;
+}
+
+void UBTService_ArePurgeZonesInRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	AAIController const* AIController = OwnerComp.GetAIOwner();
+	if (!AIController || !AIController->GetPawn()) return;
+
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp) return;
+	
+	UStudentPerceptor* PerceptorComp { AIController->GetPawn()->GetComponentByClass<UStudentPerceptor>() };
+	auto PurgeZones { PerceptorComp->GetSortedPurgeZones() };
+	
+	if (!PurgeZones.IsEmpty()) 
+	{
+		double Distance {  FVector::Dist(PurgeZones[0]->GetActorLocation(), AIController->GetPawn()->GetActorLocation()) };
+		
+		if (Distance < Range)
+		{
+			BlackboardComp->SetValueAsBool(BlackboardKey.SelectedKeyName, true);
+		}
+	}
+}
---
+++ Plugins/GameFeatures/LinKyawKhineZombie/Source/LinKyawKhineZombieRuntime/Services/BTService_ArePurgeZonesInRange.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_ArePurgeZonesInRange.generated.h"
+
+UCLASS()
+class LINKYAWKHINEZOMBIERUNTIME_API UBTService_ArePurgeZonesInRange : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	float Range;
+
+public:
+	UBTService_ArePurgeZonesInRange();
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
